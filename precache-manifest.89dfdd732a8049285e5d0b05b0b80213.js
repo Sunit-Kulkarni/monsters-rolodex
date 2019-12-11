75418548a0e3302868463d079dda070185977b92
Sunit Kulkarni <sunit@Sunits-MBP.hsd1.ga.comcast.net>
2019-12-11 00:01:26 -0500

    Updates

---
+++ precache-manifest.89dfdd732a8049285e5d0b05b0b80213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c25385fed8f38b27706fe543442a0f2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19b4fc6e45b804942523",
+    "url": "/monsters-rolodex/static/css/main.a6c252c2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19b4fc6e45b804942523",
+    "url": "/monsters-rolodex/static/js/main.cdadd7f6.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
